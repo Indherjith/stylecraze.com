17df68fb4eb7698fa99ae47e5cb515cfb71270e7
Indherjith <buvanesifet26@gmail.com>
2022-04-04 08:36:41 +0530

    final
    final

---
+++ scripts/nailArt_faceMakeup_hairStyle_navbar.js
@@ -0,0 +1,10 @@
+
+const navbar = ()=> {
+
+    return `<div id="navbar-nail-art">
+                <a href="./index.html"><img src="https://cdn2.stylecraze.com/wp-content/uploads/static-content/logos/sc-logo.png.webp" alt="logo image"></a>
+            </div>`
+
+}
+
+export default navbar
